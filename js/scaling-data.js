47fead81038a0c7504cbf148f378ecef39aa6f97
Akshay Khobragade <akshaykhobragade@Akshays-MacBook-Air.local>
2020-11-29 15:35:33 +0530

    Adding Axis to the chart

--- js/scaling-data.js
+++ js/scaling-data.js
@@ -1,24 +1,29 @@
 var h = 100;
-var w = 400;
+var w = 350;
+var padding = 20;
 
 function showHeader(ds) {
   d3.select("body")
-    .append("h1")
+    .append("h4")
     .text(ds.category + " Sales (2013)");
 }
 
+function getDate(d) {
+  var startDate = new String(d);
+  var year = startDate.substr(0, 4);
+  var month = startDate.substr(4, 2) - 1;
+  var day = startDate.substr(6, 2);
+  return new Date(year, month, day);
+}
+
 function buildLine(ds) {
-  var xScale = d3.scale
-    .linear()
-    .domain([
-      d3.min(ds.monthlySales, function (d) {
-        return d.month;
-      }),
-      d3.max(ds.monthlySales, function (d) {
-        return d.month;
-      }),
-    ])
-    .range([0, w]);
+  var minDate = getDate(ds.monthlySales[0]["month"]);
+  var maxDate = getDate(ds.monthlySales[ds.monthlySales.length - 1]["month"]);
+
+  var xScale = d3.time
+    .scale()
+    .domain([minDate, maxDate])
+    .range([padding, w - padding]);
 
   var yScale = d3.scale
     .linear()
@@ -28,12 +33,19 @@ function buildLine(ds) {
         return d.sales;
       }),
     ])
-    .range([h, 0]);
+    .range([h - padding, 10]);
+
+  var xAxisGen = d3.svg
+    .axis()
+    .scale(xScale)
+    .orient("bottom")
+    .tickFormat(d3.time.format("%b"));
+  var yAxisGen = d3.svg.axis().scale(yScale).orient("left").ticks(4);
 
   var lineFun = d3.svg
     .line()
     .x(function (d) {
-      return xScale(d.month);
+      return xScale(getDate(d.month));
     })
     .y(function (d) {
       return yScale(d.sales);
@@ -41,6 +53,18 @@ function buildLine(ds) {
     .interpolate("linear");
 
   var svg = d3.select("body").append("svg").attr({ width: w, height: h });
+
+  var yAxis = svg
+    .append("g")
+    .call(yAxisGen)
+    .attr("class", "axis")
+    .attr("transform", "translate(" + padding + ",0)");
+
+  var xAxis = svg
+    .append("g")
+    .call(xAxisGen)
+    .attr("class", "axis")
+    .attr("transform", "translate(0," + (h - padding) + ")");
 
   var viz = svg.append("path").attr({
     d: lineFun(ds.monthlySales), //we have to refernce the sales data array
